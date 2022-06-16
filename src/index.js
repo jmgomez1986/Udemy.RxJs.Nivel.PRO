b57e1d11e4b259eb7a87d8bcca261918371a367e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-16 11:12:05 -0300

    Seccion 03: 13. Funciones "range" y "of" de RxJS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //import stuff
 // import example from './01 - create';
-import example from './02 - from';
+// import example from './02 - from';
+import example from './03 - of-range';
 
 //run stuff
 example();
