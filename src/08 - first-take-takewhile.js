cc9043609db9ba01bffaa4863fd97b96275b3614
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-21 11:50:55 -0300

    Seccion 04: 18. Operadores "first", "take" y "takeWhile" de RxJS

---
+++ src/08 - first-take-takewhile.js
@@ -0,0 +1,27 @@
+import { displayLog } from './utils';
+import { fromEvent } from 'rxjs';
+import { map, first, take, takeWhile, tap } from 'rxjs/operators';
+
+// first: Emite la primerra emision del Observable
+// take: Emite las primeras x emisiones del Observable fuente
+// takeWhile: Emite las emisiones del Observable fuente siempre y hasta cuando cumplan 
+//            la condición especificada. Se completa en cuanto haya un valor que no
+//            cumpla la condición
+
+export default () => {
+	/** start coding */
+	const grid = document.getElementById('grid');
+	const click$ = fromEvent(grid, 'click').pipe(
+		map((val) => [
+      Math.floor(val.offsetX / 50),
+      Math.floor(val.offsetY / 50)
+    ]),
+		// first(val => val[0] > 3),
+		// take(4)
+		takeWhile(([col, row]) => col > 3)
+	);
+
+	const subscription = click$.subscribe((data) => displayLog(data));
+
+	/** end coding */
+};
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@
 // import example from './04 - interval-time';
 // import example from './05 - fromEvent';
 // import example from './06 - mapto-map-and-filter';
-import example from './07 - tap';
+// import example from './07 - tap';
+import example from './08 - first-take-takewhile';
 
 //run stuff
 example();
