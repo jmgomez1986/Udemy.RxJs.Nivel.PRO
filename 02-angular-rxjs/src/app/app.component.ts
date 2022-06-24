bec0985f51c316e3a33c524ada93a4a319a07361
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-06-24 17:02:55 -0300

    Seccion 11: Angular con RxJS - Unsubscribe

--- 02-angular-rxjs/src/app/app.component.ts
+++ 02-angular-rxjs/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { SearchboxComponent } from './searchbox/searchbox.component';
 import { PostsService } from './services/posts.service';
@@ -8,17 +9,26 @@ import { PostsService } from './services/posts.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('search', { static: true }) searchBox: SearchboxComponent;
 
   public posts;
+  public posts$: Observable<any>;
+  public postsSubscription: Subscription;
+
   constructor(private postsService: PostsService) {}
 
   ngOnInit(): void {
-    this.searchBox.value
-    .pipe(
+    this.posts$ = this.searchBox.value.pipe(
       switchMap((val) => this.postsService.search(val))
-    )
-    .subscribe((data) => (this.posts = data));
+    );
+
+    this.postsSubscription = this.posts$.subscribe(
+      (data) => (this.posts = data)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.postsSubscription.unsubscribe();
   }
 }
